546194b4caa466e5a0deb322aa5074db7de0da77
SAY-Hong <bobseahee@gmail.com>
2023-11-21 23:25:38 +0900

    networkManager(2)
    - BestView 코드 수정 및 코드 정리

--- NetworkPractice/BestView.swift
+++ NetworkPractice/BestView.swift
@@ -9,35 +9,33 @@ import SwiftUI
 
 struct BestView: View {
     @State private var text = "URL 받아오기 전"
-    @Binding var posts: [String]
+    @State private var users: [String] = []
     var body: some View {
         VStack(spacing: 100) {
             Text(text)
-            ForEach(posts, id: \.self) { item in
-                Text(item)
-            }
+            Text(users.first ?? "받아오지 않음")
             
             Button {
                 text = "URL 받아온 후"
-                ContentView().requestPost()
+                requestUser()
+                
             } label: {
                 Text("불러오기")
             }.buttonStyle(.bordered)
 
         }
     }
-//    func requestPost() {
-//        requestURL(urlString: postAddress) { Post, error in
-//            
-//            guard let Post = Post else {
-//                print("error")
-//                return
-//            }
-//            posts.append(Post.title)
-//        }
-//    }
+    private func requestUser() {
+        NetworkManager.shared.requestUser(urlString: NetworkManager.shared.userAddress) { user, error in
+            guard let user = user else {
+                print("error")
+                return
+            }
+            users.append(user.name)
+        }
+    }
 }
 
-//#Preview {
-//    BestView()
-//}
+#Preview {
+    BestView()
+}
