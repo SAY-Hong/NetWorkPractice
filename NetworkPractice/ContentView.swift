bf573f8e8e7bb5850fbf6cbf55ab5c6ba8f21eec
SAY-Hong <bobseahee@gmail.com>
2023-11-21 23:06:45 +0900

    networkManager(1)
    - ContentView의 requestURL(), requestUser() 를 NetWorkManager에 빼기

--- NetworkPractice/ContentView.swift
+++ NetworkPractice/ContentView.swift
@@ -89,142 +89,23 @@ struct ContentView: View {
     }
     
     private func requestPost() {
-        requestURL(urlString: postAddress) { Post, error in
-            
+        NetworkManager.shared.requestURL(urlString: postAddress) { Post, error in
             guard let Post = Post else {
                 print("error")
                 return
             }
-            
             posts.append(Post.title)
         }
     }
     
     private func requestUser() {
-        requestUser(urlString: userAddress) { user, error in
-            
+        NetworkManager.shared.requestUser(urlString: userAddress) { user, error in
             guard let user = user else {
                 print("error")
                 return
             }
-            
             users.append(user.name)
         }
-    }
-    
-    //MARK: 04 another request 영상부터 다시 보기
-    //urlString 매개변수로 넣어서 작성하기
-    func requestURL(urlString: String, completion: @escaping (Post?, String?) -> ()) {
-        //어디에 요청을 할 것인가?
-//        let urlString = "https://koreanjson.com/posts/1"
-        
-        //위에가 url인지 검증하기
-        guard let url = URL(string: urlString) else {
-            completion(nil, "This is not correct url")
-            return
-        }
-        
-        /* <URLSession>
-         - 특정 url을 이용하여 데이터를 다운로드하고 업로드하기 위한 API
-         - 앱에서 서버와 통신하기 위한 API
-         */
-        
-//        //URLRequest 설정
-//        var request: URLRequest = URLRequest(url: url)
-//        request.httpMethod = "GET"
-//        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-//        
-//        //URLSession 생성(기본 세션)
-//        let session: URLSession = URLSession(configuration: .default)
-//        
-//        session.dataTask(with: request) { data, response, error in
-//            if let _ = error {
-//                completion(nil, "We got some error. check the internet.")
-//                return
-//            }
-//            
-//            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-//                completion(nil, "Invaild response")
-//                return
-//            }
-//            
-//            guard let data = data else {
-//                completion(nil, "The data recived is wrong.")
-//                return
-//            }
-//            
-//            do {
-//                let decodedResponse = try JSONDecoder().decode(Post.self, from: data)
-//                completion(decodedResponse, nil)
-//            } catch {
-//                print(error)
-//            }
-//        }.resume()
-        
-        //URLSession 생성(기본 세션)
-        //dataTask
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            if let _ = error {
-                completion(nil, "We got some error. check the internet.")
-                return
-            }
-            
-            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                completion(nil, "Invaild response")
-                return
-            }
-            
-            guard let data = data else {
-                completion(nil, "The data recived is wrong.")
-                return
-            }
-            
-            do {
-                let decodedResponse = try JSONDecoder().decode(Post.self, from: data)
-                //data.append(decodedResponse.title)
-                completion(decodedResponse, nil)
-            } catch {
-                print(error)
-            }
-        }.resume()
-    }
-    
-    func requestUser(urlString: String, completion: @escaping (User?, String?) -> ()) {
-        //어디에 요청을 할 것인가?
-//        let urlString = "https://koreanjson.com/posts/1"
-        
-        //위에가 url인지 검증하기
-        guard let url = URL(string: urlString) else {
-            completion(nil, "This is not correct url")
-            return
-        }
-        
-        //URLSession 생성(기본 세션)
-        //dataTask
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            if let _ = error {
-                completion(nil, "We got some error. check the internet.")
-                return
-            }
-            
-            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                completion(nil, "Invaild response")
-                return
-            }
-            
-            guard let data = data else {
-                completion(nil, "The data recived is wrong.")
-                return
-            }
-            
-            do {
-                let decodedResponse = try JSONDecoder().decode(User.self, from: data)
-                //data.append(decodedResponse.title)
-                completion(decodedResponse, nil)
-            } catch {
-                print(error)
-            }
-        }.resume()
     }
 }
 
